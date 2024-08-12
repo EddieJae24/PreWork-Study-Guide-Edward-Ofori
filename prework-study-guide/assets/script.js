8ab9cbbee03ac412f65738fe1606eb3319e53d82
Edward Ofori <tetraed.llc@gmail.com>
2024-08-12 13:31:20 -0400

    Made changes to commit

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,26 +1,31 @@
-var topics = ["HTML", "CSS", "Git", "Javascript"];
+var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-//if (0) {console.log ("I am 0")} else if (null) {console.log ("I'm null")} else if (true) {console.log("I am true")};//
+function listTopics () {
+    for(var x = 0; x < topics.length; x++) 
+        {console.log (topics[x])}
+};
 
+function selectTopic () { 
+    if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
+}; 
 
-/*if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
-var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log ("Here are the topics we learned through Prework");
+listTopics ();
+console.log('Which topic should we study first?');
+selectTopic ();
+
+/*var shapes = ["triangle", "square", "pentagon", "circle"];
 console.log(shapes[0]);
 for(var x = 0; x < shapes.length; x++) {console.log(shapes[x])};*/
-
-for(var x = 0; x < topics.length; x++) {console.log(topics[x])};
-/*function makeTea () {console.log ("Heat up the water");
-    console.log ("Place the Teabag into the hot water");
-    console.log ("Pour the tea into a cup"); 
-                 }
-makeTea ();*/
+//if (0) {console.log ("I am 0")} else if (null) {console.log ("I'm null")} else if (true) {console.log("I am true")};//

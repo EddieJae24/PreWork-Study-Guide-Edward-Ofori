d919315703f1704f4222b85ff07d9f69af5da44e
Edward Ofori <tetraed.llc@gmail.com>
2024-08-12 01:19:26 -0400

    Add Javascript code to the repository

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,21 @@
+var topics = ["HTML", "CSS", "Git", "Javascript"];
+
+//if (0) {console.log ("I am 0")} else if (null) {console.log ("I'm null")} else if (true) {console.log("I am true")};//
+
+
+/*if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+for(var x = 0; x < shapes.length; x++) {console.log(shapes[x])};*/
+
+for(var x = 0; x < topics.length; x++) {console.log(topics[x])};
